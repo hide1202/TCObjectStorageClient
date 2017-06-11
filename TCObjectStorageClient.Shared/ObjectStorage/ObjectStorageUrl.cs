86173db4549b198dcb50334d5129337475ae47ea
Taegyeong Kim <hide1202@naver.com>
2017-06-12 00:25:43 +0900

    Implement progressbar. Fix bug that can't get correct urls. Fix bug that get hidden files when get files in directory.

--- TCObjectStorageClient.Shared/ObjectStorage/ObjectStorageUrl.cs
+++ TCObjectStorageClient.Shared/ObjectStorage/ObjectStorageUrl.cs
@@ -4,17 +4,17 @@
     {
         internal static string UrlForToken()
         {
-            return "https://api-compute.cloud.toast.com/identity/v2.0/tokens";
+            return $"https://api-compute.cloud.toast.com/identity/v2.0/tokens";
         }
 
         internal static string UrlForObject(string account, string containerName, string objectName)
         {
-            return "https://api-storage.cloud.toast.com/v1/{account}/{containerName}/{objectName}";
+            return $"https://api-storage.cloud.toast.com/v1/{account}/{containerName}/{objectName}";
         }
 
         internal static string UrlForContainer(string account, string containerName)
         {
-            return "https://api-storage.cloud.toast.com/v1/{account}/{containerName}";
+            return $"https://api-storage.cloud.toast.com/v1/{account}/{containerName}";
         }
     }
 }
